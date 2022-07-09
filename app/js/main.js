9ad698336e8cbeb105725587d29f0c24c541ba0e
vsulimovvv <super.sulimov2014@yandex.ru>
2022-07-09 13:20:06 +0300

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -86,6 +86,24 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   })();
 
+  (function accordion() {
+    const items = document.querySelectorAll('.accordion button');
+
+    function toggleAccordion() {
+      const itemToggle = this.getAttribute('aria-expanded');
+
+      for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+      }
+
+      if (itemToggle == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+      }
+    }
+
+    items.forEach((item) => item.addEventListener('click', toggleAccordion));
+  })();
+
   // * ===== Show Menu
   (function showMenu() {
     const menuBtn = document.querySelector('.header__toggle');
