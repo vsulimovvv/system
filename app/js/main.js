c2d69f4691c7e03be5413e582de451e9c5c4750d
vsulimovvv <super.sulimov2014@yandex.ru>
2022-07-20 14:39:53 +0300

    убрал ошибку

--- app/js/main.js
+++ app/js/main.js
@@ -36,7 +36,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     var time = '';
     $.each(eventData, function (id, data) {
-      
       console.log(id);
       $.each(data, function (index, value) {
         if (index === 'time') {
@@ -265,37 +264,40 @@ window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelectorAll(headerSelector);
     const tab = document.querySelectorAll(tabSelector);
     const content = document.querySelectorAll(contentSelector);
-    if (header) {
-      hideTabContent();
-      showTabContent();
-      function hideTabContent() {
-        content.forEach((item) => {
-          item.classList.remove('active');
-        });
-        tab.forEach((item) => {
-          item.classList.remove(activeClass);
-        });
-      }
-      function showTabContent(i = 0) {
-        content[i].classList.add('active');
-        tab[i].classList.add(activeClass);
-      }
-      header.forEach((item) => {
-        if (item) {
-          item.addEventListener('click', (e) => {
-            const target = e.target;
-            if (target.classList.contains(tabSelector.replace(/\./, ''))) {
-              tab.forEach((item, i) => {
-                if (target == item || target.parentNode == item) {
-                  hideTabContent();
-                  showTabContent(i);
-                }
-              });
-            }
+
+    header.forEach((el) => {
+      if (el) {
+        hideTabContent();
+        showTabContent();
+        function hideTabContent() {
+          content.forEach((item) => {
+            item.classList.remove('active');
+          });
+          tab.forEach((item) => {
+            item.classList.remove(activeClass);
           });
         }
-      });
-    }
+        function showTabContent(i = 0) {
+          content[i].classList.add('active');
+          tab[i].classList.add(activeClass);
+        }
+        header.forEach((item) => {
+          if (item) {
+            item.addEventListener('click', (e) => {
+              const target = e.target;
+              if (target.classList.contains(tabSelector.replace(/\./, ''))) {
+                tab.forEach((item, i) => {
+                  if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
   }
   someTabs(
     '.tabs',
