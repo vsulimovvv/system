99677181a02b13041ad669cf080e246c5e23cf44
vsulimovvv <super.sulimov2014@yandex.ru>
2022-08-14 17:58:03 +0300

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
   // * ===== Mask input
   $('input[type="tel"]').mask('+7 (999) 999-99-99');
+  $('#dob').mask('99/99/9999');
+  $('select').niceSelect();
+
+  // * ===== file upload
+  (function loadForm() {
+    let inputs = document.querySelectorAll('.input-file-upload');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.previousElementSibling,
+        labelVal = label.querySelector('.custom-file-upload span').innerText;
+
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+
+        if (countFiles)
+          label.querySelector('.custom-file-upload span').innerText =
+            'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.custom-file-upload span').innerText = labelVal;
+      });
+    });
+  })();
 
   // * Vanilla Calendar
   (function calendar() {
